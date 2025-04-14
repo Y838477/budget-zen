0fa088f6be5b2a6fe1a805e4ddb878b727202924
Y838477 <ystm.pro@gmail.com>
2025-04-14 23:34:12 +0200

    Initial commit

---
+++ src/main/java/fr/ysaintmartin/budgetzen/BudgetZenApplication.java
@@ -0,0 +1,13 @@
+package fr.ysaintmartin.budgetzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetZenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetZenApplication.class, args);
+    }
+
+}
